fa2874b36e1042d846da27088898e4e15961d6fe
Roogry <jodiemantra@gmail.com>
2025-01-25 09:17:34 +0800

    change font style on service card

--- components/service-card.vue
+++ components/service-card.vue
@@ -3,7 +3,7 @@
     <div class="py-4 px-4 flex flex-row gap-4 items-center text-center md:py-10 md:px-8 md:flex-col md:gap-6 sm:py-5 sm:px-6">
       <component :is="icon" class="shrink-0 w-8 h-8 text-black sm:w-16 sm:h-16" :fontControlled="false"/>
       <div class="service-info max-w-[300px] flex flex-col gap-1 items-start md:gap-2 md:items-center">
-        <h2 class="text-md text-start font-semibold md:text-xl md:text-center">{{ name }}</h2>
+        <h2 class="text-md text-start font-normal md:text-xl md:text-center sm:font-semibold">{{ name }}</h2>
         <p class="text-xs text-start text-secondary md:text-center sm:text-sm">{{ description }}</p>
       </div>
     </div>
