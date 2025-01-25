1cd51a9cf220224eff7f600d86b9572159782b90
Roogry <jodiemantra@gmail.com>
2025-01-25 09:17:49 +0800

    improve style on badge

--- components/badge.vue
+++ components/badge.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="cn(badgeVariants({ variant }), props.class)">
-    <div class="px-4 py-2">
+    <div class="px-3 py-2">
       <slot />
     </div>
   </div>
@@ -18,6 +18,7 @@ const badgeVariants = cva(
       variant: {
         default: "border-gray-400",
         colored: "border-blue-300/60 bg-blue-100 text-blue-700 backdrop-blur-md",
+        blur: "border-gray-400/60 bg-white/50 backdrop-blur-md",
       },
     },
     defaultVariants: {
