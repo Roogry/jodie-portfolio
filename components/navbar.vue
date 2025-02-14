91ebe02e7c03eb453a7c2cc839f9b172b980586a
Roogry <jodiemantra@gmail.com>
2025-02-14 19:45:54 +0700

    add toggle dark mode

--- components/navbar.vue
+++ components/navbar.vue
@@ -10,15 +10,36 @@
       <nav class="items-center justify-center">
         <NavMenu v-gsap.delay-500.stagger.from="{ opacity: 0 }" />
       </nav>
-      <NuxtLink v-gsap.delay-1000.from="{ opacity: 0 }" to="/#footer" class="hidden px-5 py-2 rounded-full font-semibold text-white bg-blue-600 hover:bg-blue-800 sm:inline-block dark:bg-blue-800 dark:hover:bg-blue-700">
-        Contact Me
-      </NuxtLink>
+
+      <div v-gsap.delay-1000.stagger.from="{ opacity: 0 }" class="flex items-center gap-4">
+        <UButton
+          :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'"
+          color="blue"
+          variant="ghost"
+          aria-label="Theme"
+          class="w-8 h-8 flex-items justify-center"
+          @click="isDark = !isDark"
+        />
+        <NuxtLink to="/#footer" class="hidden px-5 py-2 rounded-full font-semibold text-white bg-blue-600 hover:bg-blue-800 sm:inline-block dark:bg-blue-800 dark:hover:bg-blue-700">
+          Contact Me
+        </NuxtLink>
+      </div>
     </div>
   </header>
 </template>
 
 <script lang="ts" setup>
 import ILogo from '~/assets/logo/logo.svg'
+
+const colorMode = useColorMode()
+const isDark = computed({
+  get () {
+    return colorMode.value === 'dark'
+  },
+  set () {
+    colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
+  }
+})
 </script>
 
 <style></style>
